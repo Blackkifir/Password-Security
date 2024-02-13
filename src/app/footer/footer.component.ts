c25ad4e81a38285059b131caa8932442f308366d
Nikolay Nikiforuk <goldlamborgini@gmail.com>
2024-02-13 16:32:30 +0200

    create test-project: password security

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'password-footer',
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+}
